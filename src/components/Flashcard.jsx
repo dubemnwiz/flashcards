7f45a474dfd3b683b415c501b7c304d00da4bd22
Dubem Nwizubo <nwizugbod@gmail.com>
2025-03-08 01:50:54 -0500

    Added flip animation

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,54 +1,37 @@
 import { useState } from 'react';
 import '../styles/Flashcard.css';
 
-
-function Flashcard({flashcards}) {
+function Flashcard({ flashcards }) {
   const [index, setIndex] = useState(0);
-  const [isQuestion, toggleCard] = useState(true);
+  const [isFlipped, setFlipped] = useState(false);
 
   const nextCard = () => {
-    setIndex((index + 1) % 10);
-    toggleCard(true);
-    console.log(index);
+    setIndex((index + 1) % Object.keys(flashcards).length);
+    setFlipped(false);
   };
 
   const flipCard = () => {
-    toggleCard(!isQuestion);
+    setFlipped(!isFlipped);
   };
 
-  if (!isQuestion) {
-    return (
-      <>
-        <div className="intro">
-          <h1>From The A</h1>
-          <h3>How well do you know Atlanta's most popular artists? Test your song knowledge here!</h3>
-          <h3>Number of Cards: {Object.keys(flashcards).length}</h3>
-        </div>
-        <br></br>
-        <div className='flashcard' onClick={flipCard}> 
-          <div className='question'> {flashcards[index].answer} </div>
-        </div>
-        <br></br>
-        <button onClick={nextCard}> Next Question </button>
-      </>
-    )
-  }
-
   return (
-      <>
-        <div className="intro">
-          <h1>From The A</h1>
-          <h3>How well do you know Atlanta's most popular artists? Test your song knowledge here!</h3>
-          <h3>Number of Cards: {Object.keys(flashcards).length}</h3>
+    <>
+      <div className="intro">
+        <h1>From The A</h1>
+        <h3>How well do you know Atlanta's most popular artists? Test your song knowledge here!</h3>
+        <h3>Number of Cards: {flashcards.length}</h3>
+      </div>
+      <br />
+      <div className="flashcard-container" onClick={flipCard}>
+        <div className={`flashcard ${isFlipped ? "flipped" : ""}`}>
+          <div className="front">{flashcards[index].question}</div>
+          <div className="back">{flashcards[index].answer}</div>
         </div>
-        <br></br>
-        <div className='flashcard' onClick={flipCard}> 
-          <div className='question'> {flashcards[index].question} </div>
-        </div>
-        <br></br>
-        <button onClick={nextCard}> Next Question </button>
-      </>
-  )
+      </div>
+      <br />
+      <button onClick={nextCard}> Next Question </button>
+    </>
+  );
 }
 
 export default Flashcard;
